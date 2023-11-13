ae67c76193f7b1ccd36373bee8ff511ed811823d
atezgider <atezgider@gmail.com>
2023-11-13 22:01:54 +0100

    13.11

--- src/test/java/appium02/Test1.java
+++ src/test/java/appium02/Test1.java
@@ -12,7 +12,60 @@ public class Test1 extends BaseTest {
         click("Weiter");
         click("OK");
         click("OK");
+
+    }
+
+    @Test(dependsOnMethods = "test1")
+    public void test2(){
+        // click API Demos
         click("API Demos");
+
+        // click Views
+        click("Views");
+
+        swipeV(.8, .3);
+
+        // click TextSwitcher
+        click("TextSwitcher");
+
+        // Ekrandaki sayi 5 oluncaya kadar Next butonuna tikla
+
+        for (int i = 0; i < 5; i++) {
+            click("next");
+            waitForVisibilityOf(String.valueOf(i+1));
+        }
+
+
+
+    }
+
+
+    @Test(dependsOnMethods = "test1")
+    public void test3_swipeVerical(){
+        // click API Demos
+        click("API Demos");
+
+        // click Views
+        click("Views");
+
+        swipeV(.7, .4);
+        swipeV(.7, .4);
+        swipeV(.4, .7);
+        swipeV(.4, .7);
+
+    }
+
+    @Test(dependsOnMethods = "test1")
+    public void test4_swipeUntil(){
+        // click API Demos
+        click("API Demos");
+
+        // click Views
+        click("Views");
+
+        swipeUntilVisible("WebView", true);
+        swipeUntilVisible("Animation", false);
+
     }
 
 
--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -2,6 +2,9 @@ package base;
 
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileElement;
+import io.appium.java_client.TouchAction;
+import io.appium.java_client.touch.WaitOptions;
+import io.appium.java_client.touch.offset.PointOption;
 import org.openqa.selenium.By;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -20,37 +23,100 @@ public abstract class BaseTest {
 
 
     @BeforeTest
-    public void beforeTest(){
+    public void beforeTest() {
         driver = Driver.getDriver(Device.SAMSUNG_A33, App.APIDEMO);
         wait = new WebDriverWait(driver, 30);
     }
 
 
     @AfterTest
-    public void afterTest(){
+    public void afterTest() {
         Driver.quit();
     }
 
 
-    public void click(String text){
-        By locator = getXpathWithText(text);
+    public void click(String text) {
+        By locator = getXpathWithTextAttr(text);
         click(locator);
     }
 
 
-    public void click(By locator){
+    public void click(By locator) {
         wait.until(ExpectedConditions.elementToBeClickable(locator)).click();
     }
 
-    public void senkeys(By locator, String text){
+    public void senkeys(By locator, String text) {
         wait.until(ExpectedConditions.visibilityOfElementLocated(locator)).sendKeys(text);
     }
 
 
-    public By getXpathWithText(String text){
+    public By getXpathWithTextAttr(String text) {
         return By.xpath("//*[@text='" + text + "'] | //*[@*[contains(., '" + text + "')]]");
     }
 
+    public void waitForVisibilityOf(String text) {
+        By locator = getXpathWithTextAttr(text);
+        waitForVisibilityOf(locator);
+    }
+
+    public void waitForVisibilityOf(By locator) {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+    }
+
+    public void swipeV(double start, double end) {
+        int width = driver.manage().window().getSize().getWidth();
+        int height = driver.manage().window().getSize().getHeight();
+
+        if (start < .1) start = .1;
+        if (start > .9) start = .9;
+
+        if (end < .1) end = .1;
+        if (end > .9) end = .9;
+
+        int startPoint = (int) (height * start);
+        int endPoint = (int) (height * end);
+
+        new TouchAction<>(driver)
+                .press(PointOption.point(width / 2, startPoint))
+                .waitAction(WaitOptions.waitOptions(Duration.ofMillis(300)))
+                .moveTo(PointOption.point(width / 2, endPoint))
+                .release()
+                .perform();
+    }
+
+
+    public void swipeUntilVisible(String text, boolean down) {
+        By locator = getXpathWithTextAttr(text);
+        swipeUntilVisible(locator, down);
+    }
+
+    public void swipeUntilVisible(By locator, boolean down) {
+
+        while (true) {
+            try {
+                if (driver.findElement(locator).isDisplayed())
+                    break;
+            } catch (Exception e) {
+                if (down)
+                    swipeV(.6, .5);
+                else
+                    swipeV(.4, .5);
+            }
+        }
+    }
+
+    public void swipeUntilVisible1(By locator, boolean down) {
+
+        while (true) {
+            if (driver.findElements(locator).size() > 0)
+                break;
+
+            if (down)
+                swipeV(.6, .5);
+            else
+                swipeV(.4, .5);
+        }
+    }
 
 
     public void sleep(long millis) {
